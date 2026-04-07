c723f0fdf463970edc56979ce14cd63b490b3189
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2026-04-07 11:29:10 +0200

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    status VARCHAR(50) NOT NULL
+);
+INSERT INTO tasks (id, name, status) VALUES
+(1, 'Milk', 'done'), (2, 'Eggs', 'done'), (3, 'Bread', 'pending'), (4, 'Butter', 'pending'), (5, 'Orange juice', 'pending');
